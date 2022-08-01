159e5f17616b62814a6fb258e4410696d96afb58
Alessandro Ricci <alessandroricci@MBPdiAlessandro.homenet.telecomitalia.it>
2022-08-01 22:05:46 +0200

    js-13

---
+++ exercise-3.js
@@ -0,0 +1,42 @@
+const sum = (a, b) => {
+  return a + b;
+}
+
+const subtract = (a, b) => {
+  return a - b;
+}
+
+const multiply =  (a, b) => {
+  return a * b;
+}
+
+const divide =  (a, b) => {
+  return a / b;
+}
+
+
+/*
+let log = (value) => {
+  console.log(rdiv);
+}
+*/
+
+
+const rsum1 = sum(2,4);
+const rsum2 = sum(5,2);
+const rmolt = multiply(rsum1,rsum2);
+const rsub = subtract(rmolt,2);
+const rdiv = divide(rsub,5);
+
+
+const result = console.log(rdiv);
+
+
+
+
+
+
+
+
+
+
